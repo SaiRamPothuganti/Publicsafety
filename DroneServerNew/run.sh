a3459f024d4aa11e7bc2c69d78ef52011e696af2
SaiRamPothuganti <32654828+SaiRamPothuganti@users.noreply.github.com>
2019-08-15 13:26:15 -0400

    uploading the run.sh
    This file is called from the django restframework

---
+++ DroneServerNew/run.sh
@@ -0,0 +1,2 @@
+python C:\\Python27\\Tellov1\\Tello-Python-master\\Tello-Python-master\\Tello_Video\\TelloToFire_polygon.py &
+python C:\\Python27\\Tellov1\\Tello-Python-master\\Tello-Python-master\\Tello_Video\\main.py &
